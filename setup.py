cefa786537ea4a66b8e9b7853f39c8f2f6f7d15d
Hector Sanjuan <hector@convivencial.org>
2014-11-05 23:13:23 +0100

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-# chardet's setup.py
 from distutils.core import setup
 setup(
     name = "dccpi",
@@ -9,11 +8,16 @@ setup(
     author_email = "hector@convivencial.org",
     url = "https://github.com/hsanjuan/dccpi",
     download_url = "",
-    keywords = ["dcc", "nmra", "pi", "raspberry", "modelling", "train", "decoder" ],
+    license = "GNU General Public License v3 (GPLv3)",
+    keywords = ["dcc", "nmra", "pi", "raspberry", "modelling",
+                "train", "decoder" ],
+    install_requires=[
+        'bitarray',
+    ],
     classifiers = [
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+        "License :: OSI Approved :: "GNU General Public License v3 (GPLv3)
         "Development Status :: 1 - Alpha",
         "Environment :: Plugins",
         "Intended Audience :: Developers",
@@ -24,17 +28,5 @@ setup(
         "Topic :: System :: Networking",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-    long_description = """\
-NRMA Digical Command Control (DCC) implementation for Raspberry Pi
-------------------------------------------------------------------
-
-This module allows to use a minimal subset of the DCC protocol to
-control DCC-compatible devices, usually model trains, using the 
-Raspberry PI GPIO interface.
-
-It is based on the:
-  * [S-91 Electrical Standard](http://www.nmra.org/sites/default/files/standards/sandrp/pdf/s-9.1_electrical_standards_2006.pdf)
-  * [S-92 DCC Communications Standard](http://www.nmra.org/sites/default/files/s-92-2004-07.pdf)
-
-"""
+    long_description = read('README.md')
 )
