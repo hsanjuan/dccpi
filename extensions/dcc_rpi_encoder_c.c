8a7f46826376005605f80efb945aa3815c790705
Hector Sanjuan <hector@convivencial.org>
2014-11-09 20:38:15 +0100

    Attempt to fix c extension

--- extensions/dcc_rpi_encoder_c.c
+++ extensions/dcc_rpi_encoder_c.c
@@ -4,8 +4,8 @@
 extern void delayMicrosecondsHard (unsigned int howLong);
 
 static PyObject * dcc_rpi_encoder_c_send_bit_array(PyObject *self, PyObject *args){
-    const char *bit_array;
-    char bit_array_pos;
+    char const *bit_array;
+    char const *bit_array_pos;
     const int count;
     const unsigned int bit_one_part_duration;
     const unsigned int bit_zero_part_duration;
@@ -17,9 +17,9 @@ static PyObject * dcc_rpi_encoder_c_send_bit_array(PyObject *self, PyObject *arg
         return NULL;
 
     for (i = 0; i < count; i++){
-        bit_array_pos = *bit_array;
-        while (bit_array_pos){ //string will be null terminated
-            if (bit_array_pos++ == '0'){
+        bit_array_pos = bit_array;
+        while (*bit_array_pos){ //string will be null terminated
+            if (*bit_array_pos++ == '0'){
                 //Encode 0 with 100us for each part
                 digitalWrite(0, LOW);
                 delayMicrosecondsHard(bit_zero_part_duration);
