8a1a5e070cd3839a55a3c2105bc908abd2563302
Hector Sanjuan <code@hector.link>
2016-12-11 15:45:04 +0100

    New version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, Extension
 setup(
     name="dccpi",
     packages=["dccpi"],
-    version="1.3.0",
+    version="1.3.1",
     description="A Python NMRA DCC protocol implementation for RaspberryPi",
     author="Hector Sanjuan",
     author_email="code@hector.link",
